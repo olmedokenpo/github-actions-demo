51f7cbd3e1da33a7ae7019d8d3631dbc81654ea1
Alvaro Olmedo <olmedokenpo@gmail.com>
2023-12-02 13:03:01 +0100

    Update test-app.js

--- tests/test-app.js
+++ tests/test-app.js
@@ -8,7 +8,7 @@ chai.use(chaiHttp);
 const app = require('../index.js');
 
 describe('GET /', () => {
-  it('should respond with hello world', (done) => {
+  it('should respond with hello pito pito world', (done) => {
     chai.request(app)
       .get('/')
       .end((err, res) => {
